8e8cbaca1ee1ff2e52b65631657405614fcc1513
Serguei Kalentchouk <serguei.k@hotmail.com>
2018-02-25 00:10:01 -0500

    removed unused include

--- src/Negate.h
+++ src/Negate.h
@@ -2,8 +2,6 @@
 // Use of this source code is governed by an MIT license that can be found in the LICENSE file.
 #pragma once
 
-#include <functional>
-
 #include "Utils.h"
 
 template<typename TType>
