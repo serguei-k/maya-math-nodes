1f83eef1d1efe0b0c3dbb1477ca31ed9f8911ee4
Serguei Kalentchouk <sergueik@themill.com>
2019-01-10 17:21:04 -0500

    added missing include to fix windows build

--- src/Utils.h
+++ src/Utils.h
@@ -2,6 +2,7 @@
 // Use of this source code is governed by an MIT license that can be found in the LICENSE file.
 #pragma once
 
+#include <algorithm>
 #include <cmath>
 #include <limits>
 #include <string>
